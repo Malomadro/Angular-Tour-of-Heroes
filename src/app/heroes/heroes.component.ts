379b3ab07d618dba92b8beeb61c99267a497b260
Vlad <20vladislaw04gmail.com>
2023-03-24 17:20:25 +0200

    The hero editor

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Hero } from './../hero';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+ 
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
